5cfddb900988f3168f00616860fb74eea275ab2e
Аня Сусак <anna.i.susak@gmail.com>
2024-07-01 18:34:33 +0300

    simple photo loader and uploader

--- main.py
+++ main.py
@@ -1,11 +1,39 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request, send_file
+from werkzeug.utils import secure_filename
+import os
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploads'
+
+if not os.path.exists(app.config['UPLOAD_FOLDER']):
+    os.makedirs(app.config['UPLOAD_FOLDER'])
+
 
 @app.route('/')
-def hello():
+def index():
     return render_template('index.html')
 
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    if 'file' not in request.files:
+        return "No file part"
+
+    file = request.files['file']
+
+    if file.filename == '':
+        return "No selected file"
+
+    if file:
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return render_template('uploaded.html', filename=filename)
+
+
+@app.route('/uploads/<filename>')
+def send_image(filename):
+    return send_file(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
