a4c67c0ecd6b08604c632ff63ec833b7fc856bc0
dan <melekdan@yandex.ru>
2024-07-01 17:45:56 +0300

    templates

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, World!   Тест от дани'
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
