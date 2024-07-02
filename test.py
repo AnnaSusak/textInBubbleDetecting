ea5d4917c55d4d7099893f9177686fc21ebb2633
dan <melekdan@yandex.ru>
2024-07-02 18:56:36 +0300

    some chgs

--- test.py
+++ test.py
@@ -1,47 +1,7 @@
-import os
+import asyncio
 
 import cv2
-from google.cloud import vision
-import io
-from PIL import Image, ImageDraw, ImageFont
 
-# Установите путь к JSON-файлу учетных данных сервиса
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'lithe-willow-428116-r5-8fb0a06a023f.json'
-
-# Инициализация клиента
-client = vision.ImageAnnotatorClient()
-
-# Путь к папке с изображениями
-input_folder = 'bubbles'
-output_folder = 'painted_bubbles'
-os.makedirs(output_folder, exist_ok=True)
-
-# Шрифт для надписи (необходимо, чтобы файл шрифта был доступен)
-font_path = 'arial.ttf'  # путь к файлу шрифта
-font_size = 24
-
-# Обработка изображений
-async def process_bubble(bubble, a):
-    _, bubble_bytes = cv2.imencode('.jpg', bubble)
-    content = bubble_bytes.tobytes()
-    # Создание объекта Image для Google Vision API
-    image = vision.Image(content=content)
-
-    # Вызов функции распознавания текста
-    response = client.text_detection(image=image)
-    if not response.text_annotations:
-        return [(0, 0), (0, 0)], "", ""
-    text_annotation = response.text_annotations[0]
-    description = text_annotation.description
-    vertices = text_annotation.bounding_poly.vertices
-    minx = 1000
-    miny = 1000
-    maxx = -1000
-    maxy = -1000
-    for vertex in vertices:
-        minx = min(minx, vertex.x)
-        miny = min(miny, vertex.y)
-        maxx = max(maxx, vertex.x)
-        maxy = max(maxy, vertex.y)
-    polygon = [(minx + a[0], miny + a[1]), (maxx + a[0], maxx + a[1])]
-    return polygon, description, text_annotation.locale
+import bubbler
+image = cv2.imread('bubbles/bubble_13.jpg')
+print(asyncio.run(bubbler.process_bubble(image, (0, 0))))
