dd02d42b40d0cbdf106db9dad45cc6b172b3469d
NELKIO <105226748+NELKIO@users.noreply.github.com>
2024-07-05 10:33:21 +0300

    new slice model ; conf => 60

--- bubbler.py
+++ bubbler.py
@@ -1,57 +1,218 @@
+import asyncio
+from PIL import Image, ImageDraw
+from flask import Flask, render_template, request, send_file, url_for, jsonify
+from werkzeug.utils import secure_filename
 import os
-
-import cv2
-from google.cloud import vision
-import io
-from PIL import Image, ImageDraw, ImageFont
 from roboflow import Roboflow
+import supervision as sv
+import cv2
+import bubbler
+from google.cloud import translate_v2 as translate
+import numpy as np
+from openai import OpenAI
+import json
+import requests
 
-# Установите путь к JSON-файлу учетных данных сервиса
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploads'
+app.config['BUBBLES_FOLDER'] = 'bubbles'
+app.config['TEMP_FOLDER'] = 'temp'
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'lithe-willow-428116-r5-8fb0a06a023f.json'
-rf = Roboflow(api_key="hlfVQnRCETvjVCNGVJoh")
-project = rf.workspace().project("bubble-text-detector")
-model = project.version(3).model
+# Ensure the upload and bubbles directories exist
+if not os.path.exists(app.config['UPLOAD_FOLDER']):
+    os.makedirs(app.config['UPLOAD_FOLDER'])
 
-# Инициализация клиента
-client = vision.ImageAnnotatorClient()
+if not os.path.exists(app.config['BUBBLES_FOLDER']):
+    os.makedirs(app.config['BUBBLES_FOLDER'])
 
-# Путь к папке с изображениями
-input_folder = 'bubbles'
-output_folder = 'painted_bubbles'
-os.makedirs(output_folder, exist_ok=True)
-
-# Шрифт для надписи (необходимо, чтобы файл шрифта был доступен)
-font_path = 'arial.ttf'  # путь к файлу шрифта
-font_size = 24
-
-# Обработка изображений
-async def process_bubble(bubble, a):
-    _, bubble_bytes = cv2.imencode('.jpg', bubble)
-    content = bubble_bytes.tobytes()
-    temp_path = 'temp/temp.jpg'
-    with open(temp_path, 'wb') as f:
-        f.write(content)
-    polygon = [[(a[0] + int(i['x']) - i['width'] // 2, a[1] + int(i['y']) - i['height'] // 2), (a[0] + int(i['x']) + i['width'] // 2, a[1] + int(i['y']) + i['height'] // 2)] for i in model.predict(temp_path, confidence=50, overlap=30).json()['predictions']]
-    # Создание объекта Image для Google Vision API
-    image = vision.Image(content=content)
+# Ensure the temp directory exists
+if not os.path.exists(app.config['TEMP_FOLDER']):
+    os.makedirs(app.config['TEMP_FOLDER'])
 
 
-    # Вызов функции распознавания текста
-    response = client.text_detection(image=image)
-    if not response.text_annotations:
-        print("ERROR", polygon)
-        return None
-    text_annotation = response.text_annotations[0]
-    description = text_annotation.description
-    vertices = text_annotation.bounding_poly.vertices
-    minx = 1000
-    miny = 1000
-    maxx = -1000
-    maxy = -1000
-    for vertex in vertices:
-        minx = min(minx, vertex.x)
-        miny = min(miny, vertex.y)
-        maxx = max(maxx, vertex.x)
-        maxy = max(maxy, vertex.y)
-    polygon2 = [(minx + a[0], miny + a[1]), (maxx + a[0], maxy + a[1])]
-    return polygon, description, text_annotation.locale, polygon2
+with open('luka_api_keys.json', 'r', encoding='utf-8') as file:
+    api_keys = json.load(file)
+
+# Initialize Roboflow model
+# rf = Roboflow(api_key=api_keys['roboflow'])
+# project = rf.workspace().project("manwgaspeechbubble")
+# model = project.version(1).model
+rf = Roboflow(api_key=api_keys['roboflow'])
+project = rf.workspace().project("cut_the_bubble")
+model = project.version(1).model
+
+client = OpenAI(
+    api_key=api_keys['chat_gpt']
+)
+
+url = "https://iam.api.cloud.yandex.net/iam/v1/tokens"
+data = {
+    "yandexPassportOauthToken": api_keys['yandex']
+}
+
+yandex_token = 'Bearer ' + requests.post(url, json=data).json()['iamToken']
+
+
+def get_average_color(image, x1, y1, x2, y2):
+    height, width, _ = image.shape
+    x1 = max(1, x1)
+    y1 = max(1, y1)
+    x2 = min(width - 2, x2)
+    y2 = min(height - 2, y2)
+   
+    colors = []
+    for x in range(x1 - 1, x2 + 2):
+        colors.append(image[y1 - 1, x])
+        colors.append(image[y2 + 1, x])
+        
+    for y in range(y1, y2 + 1):
+        colors.append(image[y, x1 - 1])
+        colors.append(image[y, x2 + 1])
+        
+    colors = np.array(colors)
+    average_color = np.mean(colors, axis=0)
+    
+    return np.round(average_color).astype(int)[::-1]
+
+def predicter(path):
+    result = model.predict(path, confidence=60, overlap=30).json()
+    predictions = [(pred['x'], pred['y'], pred['width'], pred['height'], pred['confidence']) for pred in
+                   result['predictions']]
+
+    # Load the image
+    image = cv2.imread(path)
+
+    tasks = []
+    for i, (x, y, width, height, confidence) in enumerate(predictions):
+        x1 = int(x - width / 2)
+        y1 = int(y - height / 2)
+        x2 = int(x + width / 2)
+        y2 = int(y + height / 2)
+
+        # Crop the bubble from the image
+        bubble = image[y1:y2, x1:x2]
+        tasks.append(bubbler.process_bubble(bubble, (x1, y1)))
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    results = loop.run_until_complete(asyncio.gather(*tasks, return_exceptions=True))
+    loop.close()
+    while None in results:
+        results.remove(None)
+    for i, result in enumerate(results):
+        print("RESULT", result)
+        bubble_info = create_overlays_for_bubble(image, result[0], str(i))
+        for j, coord in enumerate(result[0]):
+            coord.append(bubble_info[j]['filename'])
+            result[0][j] = coord
+        print(result)
+        results[i] = result
+    return results
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    if 'file' not in request.files:
+        return "No file part"
+
+    file = request.files['file']
+
+    if file.filename == '':
+        return "No selected file"
+
+    if file:
+        filename = secure_filename(file.filename)
+        path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(path)
+        bubbles = predicter(path)
+        print(bubbles)
+        return render_template('uploaded.html', filename=filename, bubbles=bubbles)
+
+def create_overlays_for_bubble(image, coordinates, bubble_name):
+    print("COORDINATES", coordinates)
+    overlay_filenames = []
+
+    if not os.path.exists(app.config['TEMP_FOLDER']):
+        os.makedirs(app.config['TEMP_FOLDER'])
+
+    for i, coords in enumerate(coordinates):
+        coords = [(int(x), int(y)) for x, y in coords]
+        print(coords)
+        
+        overlay = Image.new('RGBA', (coords[1][0] - coords[0][0], coords[1][1] - coords[0][1]), (*get_average_color(image, coords[0][0], coords[0][1], coords[1][0], coords[1][1]), 255))
+        overlay_filename = f"{os.path.splitext(bubble_name)[0]}_overlay_{i}.png"
+        overlay_path = os.path.join(app.config['TEMP_FOLDER'], overlay_filename)
+        overlay.save(overlay_path)
+        overlay_filenames.append({'filename': overlay_filename, 'coords': coords})
+
+    return overlay_filenames
+
+@app.route('/uploads/<filename>')
+def send_uploaded_file(filename):
+    return send_file(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+@app.route('/temp/<filename>')
+def send_temp_file(filename):
+    return send_file(os.path.join(app.config['TEMP_FOLDER'], filename))
+
+translate_client = translate.Client()
+
+
+@app.route('/translate', methods=['POST'])
+def translate():
+    engine = request.args.get('engine')
+    from_lang = request.args.get('from')
+    to_lang = request.args.get('to')
+    data = request.get_json()
+    text = data.get('text')
+
+    if from_lang == to_lang:
+        return jsonify({'translation': text})
+    
+
+    if engine == 'ChatGPT3.5':
+        try:
+            response = client.chat.completions.create(
+            model="gpt-3.5-turbo-1106",
+            messages=[
+            {"role": "system", "content": "You are a translator. Say only translated words without quotes"},
+            {"role": "user", "content": f"translate:\n\"{text}\"\nfrom \"{from_lang}\" to \"{to_lang}\" language"}],
+            temperature=1,
+            max_tokens=256,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0)
+            # print(response)
+            return jsonify({'translation': response.choices[0].message.content})
+        except Exception:
+            return jsonify({'translation': text})
+        
+    if engine == 'google_translate':
+        result = translate_client.translate(text, source_language=from_lang, target_language=to_lang)
+        return jsonify({'translation': result['translatedText']})
+
+    if engine == 'yandex_translate':
+        body = {
+            "targetLanguageCode": to_lang,
+            "texts": [text],
+            "folderId": 'b1g63hjg7a4mld5anvbn',
+        }
+    
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": yandex_token
+        }
+
+        response = requests.post('https://translate.api.cloud.yandex.net/translate/v2/translate',
+            json = body,
+            headers = headers
+        )
+        return jsonify({'translation': response.json()['translations'][0]['text']})
+
+    return jsonify({'error': 'Unsupported translation engine'}), 400
+
+if __name__ == '__main__':
+    app.run(debug=True)
