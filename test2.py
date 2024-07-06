078ec88377034bcb868204e10e8558834a6480d1
NELKIO <105226748+NELKIO@users.noreply.github.com>
2024-07-06 21:00:35 +0300

    Delete test2.py

--- test2.py
+++
@@ -1,6 +0,0 @@
-from roboflow import Roboflow
-
-rf = Roboflow(api_key="hlfVQnRCETvjVCNGVJoh")
-project = rf.workspace().project("bubble-text-detector")
-model = project.version(3).model
-print(model.predict('bubbles/bubble_13.jpg', confidence=50, overlap=30).json()['predictions'])
